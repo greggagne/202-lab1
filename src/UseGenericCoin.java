f9a5e26b3511a26ab30133c9b70260d74f3450bf
greggagne <ggagne@westminsteru.edu>
2023-08-09 15:44:38 -0600

    Initial commit

---
+++ src/UseGenericCoin.java
@@ -0,0 +1,16 @@
+/**
+ * @author YOUR NAME GOED HERE
+ * 
+ * Some comments about what this code does.
+ * 
+ */
+
+public class UseGenericCoin {
+
+	public static void main(String[] args) {
+
+
+	}
+		
+
+}

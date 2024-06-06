44fc110139e9eb477ccc80f26003c4af9ca556bd
greggagne <ggagne@westminsteru.edu>
2024-06-06 16:16:17 -0600

    Initial commit.

---
+++ src/GenericCoin.java
@@ -0,0 +1,10 @@
+/**
+ * @author YOUR NAME GOES HERE
+ *
+ * Some comments about what this code does.
+ */
+public class GenericCoin {
+	public enum CoinSide {HEADS, TAILS};
+	
+
+}
---
+++ src/UseGenericCoin.java
@@ -0,0 +1,14 @@
+/**
+ * @author YOUR NAME GOES HERE
+ * 
+ * Some comments about what this code does.
+ * 
+ */
+
+public class UseGenericCoin {
+
+	public static void main(String[] args) {
+
+
+	}
+}
